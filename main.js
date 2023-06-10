15f1893196614b5669cb39127a2381389cca7655
MarkoDavkovski <marko.davkovski@gmail.com>
2023-06-10 11:48:34 +0200

    Linked List structure

---
+++ main.js
@@ -0,0 +1,16 @@
+const Node = (value = null, nextNode = null) => {
+	return { value, nextNode };
+};
+
+const LinkedList = () => {
+	const prepend = () => {};
+	const append = () => {};
+	const at = (index) => {};
+	const pop = () => {};
+	const contains = (value) => {};
+	const find = (value) => {};
+	const toString = () => {};
+	const size = () => {};
+
+	return { prepend, append, at, contains, pop, find, toString, size };
+};
